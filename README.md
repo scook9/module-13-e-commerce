b69fb10c12c1688139344d60c8acf481077f1880
[Spencer Cook] <spencerrcook@hotmail.com>
2024-01-08 17:03:43 -0700

    added sequelize

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # E-commerce Back End Starter Code
+
+- Starter code cloned from https://github.com/coding-boot-camp/fantastic-umbrella
+- server.js sequelize logic copied from activity 13-ORM, 11-Ins_RESTful-Routes
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const categoryRoutes = require("./category-routes");
+const productRoutes = require("./product-routes");
+const tagRoutes = require("./tag-routes");
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+//need to update these routes in each of their respective .js files
+//http://localhost:3001/api/
+router.use("/categories", categoryRoutes);
+router.use("/products", productRoutes);
+router.use("/tags", tagRoutes);
 
 module.exports = router;
